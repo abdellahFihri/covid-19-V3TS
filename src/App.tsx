8744d3a14c4e451a231079ceab4ac2a4918efb4e
abdellahfihri <abdellahfihri@gmail.com>
2021-01-10 17:12:31 +0100

    dispatch in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.scss";
 import NavLinks from "./navbar/navbar";
-
 import MainPage from "./pages/mainPage";
 import DetailedCountriesList from "./pages/DetailedCountriesListPage";
-
 import Spinner from "./hoc/spinner/spinner";
 import CompareCountries from "./pages/compareCountriesPage";
 import Container from "./hoc/container/container";
-
 import Overlay from "./hoc/overlay/overlay";
+import DataErr from "./hoc/noData/messageError";
+import NotFound from "./pages/notFound";
+import Footer from "./footer/footer";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import {
@@ -19,71 +18,53 @@ import {
   selectLoading,
 } from "./redux/reducers/world/worldDataSelector";
 import { fetchData } from "./redux/actions/index";
-
-import { Props, State } from "./utils/intefaces/interfaces";
+import { Props } from "./utils/intefaces/interfaces";
 import { selectOverlay } from "./redux/reducers/overlay/overlaySelector";
-import Footer from "./footer/footer";
-
 import { selectFetchErrorMsg } from "./redux/reducers/fetchingError/fetchingErrorSelector";
-import DataErr from "./hoc/noData/messageError";
+import "./App.scss";
 
-import NotFound from "./pages/notFound";
+const App: React.FunctionComponent<Props> = (props) => {
+  const { loading, overlay, fetchError, selectedCountry, onFetchData } = props;
 
-require("dotenv").config();
+  useEffect(() => {
+    onFetchData();
+  }, [onFetchData]);
 
-class App extends Component<Props, State> {
-  componentDidMount() {
-    this.props.onFetchData();
-  }
+  return loading ? (
+    <Spinner title="Fetching initial data" />
+  ) : fetchError ? (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <DataErr errMsg={fetchError} iconFill="#ff0000" />
+    </div>
+  ) : (
+    <div className="App">
+      {overlay ? <Overlay /> : ""}
+      <NavLinks />
 
-  render() {
-    const { selectedCountry } = this.props;
-    const { loading, overlay, fetchError } = this.props;
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <MainPage />} />
+          <Route
+            exact
+            path={`/${selectedCountry}`}
+            render={() => <MainPage />}
+          />
+          <Route path="/countries-list" component={DetailedCountriesList} />
+          <Route path="/compare-countries" component={CompareCountries} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Container>
 
-    return loading ? (
-      <Spinner title="Fetching initial data" />
-    ) : fetchError ? (
-      <div
-        style={{
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <DataErr errMsg={fetchError} iconFill="#ff0000" />
-      </div>
-    ) : (
-      <div className="App">
-        {overlay ? <Overlay /> : ""}
-        <NavLinks />
-
-        <Container>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <MainPage
-                // executeScroll={this.executeScroll}
-                />
-              )}
-            />
-            <Route
-              exact
-              path={`/${selectedCountry}`}
-              render={() => <MainPage />}
-            />
-            <Route path="/countries-list" component={DetailedCountriesList} />
-            <Route path="/compare-countries" component={CompareCountries} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </Container>
-
-        <Footer />
-      </div>
-    );
-  }
-}
+      <Footer />
+    </div>
+  );
+};
 
 const mapDispatchprops = (dispatch: any) => {
   return { onFetchData: () => dispatch(fetchData()) };
